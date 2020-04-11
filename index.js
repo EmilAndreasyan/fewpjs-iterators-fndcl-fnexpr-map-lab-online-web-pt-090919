c72c4bda5b828d2ac8af1d1f0f24766d02892198
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-11 03:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ const tutorials = [
 
 
 const titleCased = (input) => {
+  return tutorials.map(tutorial => {
   let splitted = tutorial.split(' ')
-  let capitalized = splitted.map(split => split.charAt(0).toUpperCase + split.slice(1));
+  let capitalized = splitted.map(word => word.charAt(0).toUpperCase + word.slice(1));
   let result = capitalized.join(' ')
   return result;
 })
+}
+
+
 }
 
